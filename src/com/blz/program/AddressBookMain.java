a1556abca733916241b463151bb5d2b232f2da26
Priya <Priyamadiwalar0007@gmail.com>
2022-03-18 22:33:40 +0530

    Added:Method to read and write the address book with persons contac…

--- src/com/blz/program/AddressBookMain.java
+++ src/com/blz/program/AddressBookMain.java
@@ -1,7 +1,77 @@
 package com.blz.program;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book");
+	Scanner scanner = new Scanner(System.in);
+
+	public void chooseOption() {
+		AddressBookService addressBookService = new AddressBookService();
+
+		boolean isExit = false;
+		while (!isExit) {
+			System.out.print(
+					"Enter Number As Per Your Choice:\n1.Add-New Contacts:\n2.Display-All Records:\n3.Edit Contact\n"
+							+ "\n4.Delete Contact\n5.Create a file:\n6.Write into file\n7.Read a file\n8.Write to csv file\n9.Read from csv file\n10.Write into JSON\n11.Read from Json\n12.Exit\n");
+			int userInput = scanner.nextInt();
+			switch (userInput) {
+			case 1:
+				// add new contact.
+				addressBookService.addDetails();
+				break;
+			case 2:
+				// display all contact.
+				addressBookService.displayContacts();
+				break;
+			case 3:
+				// edit existing contact.
+				addressBookService.editContact();
+				break;
+			case 4:
+				// delete existing contact.
+				addressBookService.deleteContact();
+				break;
+			case 5:
+				// Create a file
+				addressBookService.fileCreate();
+				break;
+			case 6:
+				// Write into file
+				addressBookService.fileWrite();
+				break;
+			case 7:
+				// read from file
+				addressBookService.fileReader();
+				break;
+			case 8:
+				// write to csv file.
+				addressBookService.writeToCsv();
+				break;
+			case 9:
+				// read from csv file.
+				addressBookService.readFromCsvFile();
+				break;
+			case 10:
+				// read from csv file.
+				addressBookService.writeToJson();
+				break;
+			case 11:
+				// read from csv file.
+				addressBookService.readFromJson();
+				break;
+			case 12:
+				// Exit from program.
+				isExit = true;
+				break;
+			default:
+				System.out.println("Please enter valid input");
+			}
+		}
 	}
+
+	public static void main(String[] args) {
+		AddressBookMain addressBookMain = new AddressBookMain();
+		addressBookMain.chooseOption();
+	}
+
 }
